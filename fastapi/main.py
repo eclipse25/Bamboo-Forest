bfd9d9658f646fa2d256404a6cc3690fd7d5d082
eclipse-34 <yk99902@naver.com>
2024-05-11 22:55:09 +0900

    Add sqlite3 Database and Chores

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+import sqlite3
 
 # FastAPI 애플리케이션 인스턴스 생성
 app = FastAPI()
+
+# SQLite 데이터베이스 연결
+conn = sqlite3.connect('database.db')
+
 
 # 라우트 정의
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
 
 # 애플리케이션 실행
 if __name__ == "__main__":
