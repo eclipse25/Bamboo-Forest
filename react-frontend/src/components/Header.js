8155ff7160906e4963e248820abad6c2d44abc1e
eclipse-34 <yk99902@naver.com>
2024-05-15 03:46:14 +0900

    Componentize header

---
+++ react-frontend/src/components/Header.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
+import debounce from 'lodash.debounce';
+import '../styles/Header.css';
+
+function Header() {
+    // 검색어 상태와 검색 결과 상태를 관리하는 useState 훅 사용
+    const [searchTerm, setSearchTerm] = useState(''); // 검색어 상태
+    const [searchResults, setSearchResults] = useState([]); // 검색 결과 상태
+    const [isFocused, setIsFocused] = useState(false); // 입력 필드의 포커스 상태
+    const navigate = useNavigate();
+
+    // Debounced handleSearch 함수
+    const debouncedSearch = useCallback(
+        debounce(async (query) => {
+            if (query.length >= 2) {
+                try {
+                    const response = await fetch(`http://localhost:8000/api/schools/?school_name=${query}`);
+                    if (!response.ok) {
+                        throw new Error('Search failed');
+                    }
+                    const data = await response.json();
+                    setSearchResults(data); // 직접 data 사용
+                } catch (error) {
+                    console.error(error);
+                    setSearchResults([]);
+                }
+            } else {
+                setSearchResults([]);
+            }
+        }, 300),
+        []
+    );
+
+    useEffect(() => {
+        debouncedSearch(searchTerm);
+        return () => {
+            debouncedSearch.cancel();  // 컴포넌트 언마운트 시 디바운싱 취소
+        };
+    }, [searchTerm, debouncedSearch]);
+
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const clearSearch = () => {
+        setSearchTerm('');
+        setSearchResults([]);
+    };
+
+    const handleSchoolClick = (school_code) => {
+        navigate(`/board/${school_code}`);
+    };
+
+    return (
+        <div id="header" role="banner">
+            <nav>
+                <div className="container bdr">
+                    <div className="nav-header bdr">
+                        <h1 className="title bdr">
+                            <Link to="/" style={{ textDecoration: 'none', color: 'inherit' }}>Bada</Link>
+                        </h1>
+                    </div>
+                    <label className="search">
+                        {!isFocused && <span className="search-icon"><FontAwesomeIcon icon={faSearch} size="1x" /></span>}
+                        <input
+                            className="search-bar"
+                            value={searchTerm}
+                            onChange={handleSearch}
+                            onFocus={() => setIsFocused(true)}
+                            onBlur={() => setIsFocused(false)}
+                            placeholder="학교 이름으로 게시판 검색"
+                            aria-label="Search input"
+                        />
+                        {isFocused && (
+                            <span className="clear-icon" onClick={clearSearch} role="button" tabIndex="0" style={{ cursor: 'pointer' }}
+                                  aria-label="Clear search">
+                                <FontAwesomeIcon icon={faTimes} size="1x" />
+                            </span>
+                        )}
+                        {searchResults.length > 0 && (
+                            <ul className="dropdown">
+                                {searchResults.map((result, index) => (
+                                    <li key={index} onClick={() => handleSchoolClick(result.school_code)}>
+                                        <div className="result-title">{result.school_name}</div>
+                                        <div className="result-address">{result.address}</div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
+                    </label>
+                    <ul className="nav-menu bdr">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
--- react-frontend/src/pages/Board.js
+++ react-frontend/src/pages/Board.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import '../styles/Board.css';
 import { useParams } from 'react-router-dom';
+import Header from '../components/Header';
 
 function Board() {
     const { school_code } = useParams();  // URL 파라미터 접근 방식
 
     return (
         <div>
-            <h1>게시판: {school_code}</h1>
-            <p>이곳은 {school_code}의 게시판입니다.</p>
-            {/* 추가적인 게시판 내용 또는 기능 구현 */}
+            <Header />
+            <div className='content bdr'>
+                <h1>게시판: {school_code}</h1>
+                <p>이곳은 {school_code}의 게시판입니다.</p>
+                {/* 추가적인 게시판 내용 또는 기능 구현 */}
+            </div>
         </div>
     );
 }
