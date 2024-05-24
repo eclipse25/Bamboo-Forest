080da3d6d78df6375304796b51143040f025f2da
eclipse-34 <yk99902@naver.com>
2024-05-24 16:39:15 +0900

    Implement Homepage

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import Menu from '../components/Menu';
+import PostList from '../components/PostList';
 import '../styles/Home.css';
 
 function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            try {
+                const response = await fetch(`http://localhost:8000/api/posts/all`);
+                const data = await response.json();
+                setPosts(data.posts);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        }
+
+        fetchPosts();
+    }, []);
+
     return (
         <div>
             <Header />
-            <main className='bdr'>
-                <p className="description">Welcome to the home page!</p>
-            </main>
+            <div className='content'>
+                <Menu />
+                <div className='board noto-sans-kr-400'>
+                    <div className='board-header'>
+                            <div className='board-info'>
+                                <h2 className='board-title'>모든 포스트</h2>
+                                <div className='board-detail'></div>
+                            </div>
+                            <div className='board-state'>
+                                <span className='posttip'>게시글 작성은 각 게시판에서!</span>
+                                <span>정렬기준</span>
+                            </div>
+                    </div>
+                    <div className='board-posts'>
+                        <PostList posts={posts} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
     }
